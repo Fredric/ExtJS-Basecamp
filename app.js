194bd672c77e8a8b7371414877340666d9e8f565
Fredric Berling <fredric.berling@gmail.com>
2012-11-29 22:56:17 +0100

    New location of ExtJS

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-Ext.Loader.setPath('Ext', '../ext/extjs-4.1.3/src');
+Ext.Loader.setPath('Ext', 'lib/ext/src');
 Ext.require('Ext.util.History');
 
 Ext.application({
     name: 'BASECAMP',
     autoCreateViewport: false,
-    controllers: ['Login', 'Navigation', 'Projects', 'TodoLists', 'Topics', 'Uploads', 'Documents'],
+
+    controllers: [
+        'Login',
+        'Navigation',
+        'Projects',
+        'TodoLists',
+        'Topics',
+        'Uploads',
+        'Documents'
+    ],
+
     launch: function () {
         Ext.util.History.init();
-
         this.getController('Login').checkLogin();
-
-
     }
-
-
 });
