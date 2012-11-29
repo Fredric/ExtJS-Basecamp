fa282bb67c73bcb55d50249cd3c7b0e7b6f52134
Fredric Berling <fredric.berling@gmail.com>
2012-11-30 00:07:24 +0100

    New events in enter press and button click.

--- app/view/window/Login.js
+++ app/view/window/Login.js
@@ -1,12 +1,44 @@
-Ext.define('BASECAMP.view.window.Login',{
-    extend:'Ext.Window',
-    alias:'widget.login',
-    bodyStyle:'padding:10px',
-    items:[
-        {itemId:'username', xtype:'textfield', fieldLabel:'User name'},
-        {itemId:'password', xtype:'textfield', fieldLabel:'Password', inputType:'password'}
-    ],
-    buttons:[
-        {text:'Login'}
-    ]
+Ext.define('BASECAMP.view.window.Login', {
+    extend: 'Ext.Window',
+    alias: 'widget.login',
+    bodyStyle: 'padding:10px',
+    initComponent: function () {
+        var me = this;
+
+        me.items = [
+            {
+                itemId: 'username',
+                xtype: 'textfield',
+                fieldLabel: 'User name'
+            },
+            {
+                itemId: 'password',
+                xtype: 'textfield',
+                fieldLabel: 'Password',
+                inputType: 'password',
+                enableKeyEvents: true,
+                listeners: {
+                    keypress: function (field, e) {
+                        if (e.getKey() === 13) {
+                            me.fireEvent('enterPressInField', me);
+                        }
+                    }
+                }
+            }
+        ];
+
+        me.buttons = [
+            {
+                text: 'Login',
+                handler: function () {
+                    me.fireEvent('loginButtonClicked', me);
+                }
+            }
+        ];
+
+        me.callParent();
+        me.on('show', function(){
+            me.down('#username').focus(true, 100);
+        });
+    }
 });
