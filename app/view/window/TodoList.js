19cecd97a3660903f078fa3a023b382b040e2c49
Fredric Berling <fredric.berling@gmail.com>
2012-12-02 18:54:44 +0100

    added modal

---
+++ app/view/window/TodoList.js
@@ -0,0 +1,42 @@
+Ext.define('BASECAMP.view.window.TodoList', {
+    extend: 'Ext.Window',
+    height:300,
+    width:800,
+    alias: 'widget.todolistwindow',
+    closeAction:'hide',
+    requires:['Ext.form.Panel'],
+    bodyStyle: 'padding:10px',
+    layout:'fit',
+    initComponent: function () {
+        var me = this;
+
+        me.remaining = Ext.create('BASECAMP.view.grid.Todos',{title:'Remaining'});
+        me.completed = Ext.create('BASECAMP.view.grid.Todos', {title:'Completed'});
+
+
+        me.items = [
+
+            {
+
+                xtype:'tabpanel',
+                items:[
+                    me.remaining,
+                    me.completed
+                ]
+
+            }
+        ];
+
+        me.buttons = [
+            {
+                text: 'Close',
+                handler: function () {
+                    me.close();
+                }
+            }
+        ];
+
+        me.callParent();
+
+    }
+});
